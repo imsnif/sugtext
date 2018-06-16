2f6d4c6c27cd18281b5f3a4cf3a4975cdf91b249
Aram Drevekenin <grimsniffer@gmail.com>
2018-06-16 18:08:40 +0200

    test(completion): can suggest and complete in the middle of a text field

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -23,6 +23,27 @@ test('contentEditable - suggest completions', async t => {
   }
 })
 
+test('contentEditable - suggest completions in the middle of a text field', async t => {
+  t.plan(1)
+  try {
+    const { browser, contentPage } = await loadExtension('one-contenteditable-field')
+    await contentPage.type('#completeme', 'foo thi bar')
+    const textboxEl = await contentPage.$('#completeme')
+    for (let i = 0; i < 4; i++) {
+      await textboxEl.press('ArrowLeft')
+    }
+    await contentPage.type('#completeme', 'a')
+    await new Promise(resolve => setTimeout(resolve, 100)) // suggestions received in under 100ms
+    const captured = await contentPage.screenshot()
+    const truth = fs.readFileSync(`${__dirname}/screenshots/contenteditable-suggest-completions-middle.png`)
+    const matchesScreenshot = await looksSame(truth, captured)
+    t.ok(matchesScreenshot, 'captured screenshot matches saved screenshot')
+    await browser.close()
+  } catch (e) {
+    t.fail(e.message)
+  }
+})
+
 test('contentEditable - complete first words when pressing TAB', async t => {
   t.plan(1)
   try {
@@ -39,3 +60,25 @@ test('contentEditable - complete first words when pressing TAB', async t => {
     t.fail(e.message)
   }
 })
+
+test('contentEditable - complete first words when pressing TAB in the middle of a text field', async t => {
+  t.plan(1)
+  try {
+    const { browser, contentPage } = await loadExtension('one-contenteditable-field')
+    await contentPage.type('#completeme', 'foo thi bar')
+    const textboxEl = await contentPage.$('#completeme')
+    for (let i = 0; i < 4; i++) {
+      await textboxEl.press('ArrowLeft', {delay: 100})
+    }
+    await contentPage.type('#completeme', 'a', {delay: 100})
+    await contentPage.keyboard.press('Tab', {delay: 100})
+    const captured = await contentPage.screenshot()
+    const truth = fs.readFileSync(`${__dirname}/screenshots/contenteditable-complete-first-word-middle.png`)
+    const matchesScreenshot = await looksSame(truth, captured)
+    t.ok(matchesScreenshot, 'captured screenshot matches saved screenshot')
+    await browser.close()
+  } catch (e) {
+    t.fail(e.message)
+  }
+})
+
