b9fc9aade5bc9271f1167a35066ecf52bfb776d6
Aram Drevekenin <grimsniffer@gmail.com>
2018-06-29 18:16:45 +0200

    refactor(update-queue): remove unneeded logic

--- js/update-queue.js
+++ js/update-queue.js
@@ -1,10 +1,6 @@
 module.exports = (store, app) => {
-  let updating
   return (path, value) => {
     store.set(path, value)
-    updating || (updating = window.requestAnimationFrame(() => {
-      updating = false
-      app.update(store.get())
-    }))
+    window.requestAnimationFrame(() => app.update(store.get()))
   }
 }
