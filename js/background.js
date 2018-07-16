cb3e43c4405c32d3044785c8f630896f92ced998
Aram Drevekenin <grimsniffer@gmail.com>
2018-07-16 14:17:47 +0200

    feat(background): use pouchdb

--- js/background.js
+++ js/background.js
@@ -1,34 +1,38 @@
-const dictWords = require('../words.json')
+'use strict'
+
+const commonEnglishWords = require('../common-words.json')
+const PouchDB = require('pouchdb')
+const db = new PouchDB('words')
+
+function initDb () {
+  // TODO: clear db?
+  const wordDocs = commonEnglishWords.map((w, i) => {
+    return {_id: w, score: commonEnglishWords.length - i}
+  })
+  return db.bulkDocs(wordDocs)
+}
+initDb()
 
 browser.runtime.onMessage.addListener(generateSuggestions)
 
 async function generateSuggestions (message) {
   const {searchterm} = message
+  // TODO: unique id from instance rather than active tab with nested iframes
   const tabs = await browser.tabs.query({active: true, windowId: browser.windows.WINDOW_ID_CURRENT})
   const tabId = tabs[0].id
   const suggestions = searchterm && searchterm.length > 1
-    ? findWords(searchterm)
+    ? await findWords(searchterm)
     : []
   if (suggestions.length > 0) {
     browser.tabs.sendMessage(tabId, {suggestions})
   }
 }
 
-
-function findWords (searchterm) {
-  // TODO: better algorithm
-  let matches = []
-  let index = 0
-  while (matches.length < 5) {
-    const word = dictWords[index]
-    if (!word) {
-      return matches
-    }
-    if (word.startsWith(searchterm)) {
-      matches.push(word)
-    }
-    index++
-  }
-  return matches
+async function findWords (searchterm) {
+  const matches = await db.allDocs({
+    startkey: searchterm,
+    endkey: `${searchterm}\uffff`,
+    include_docs: true
+  })
+  return matches.rows.sort(m => m.score).slice(0, 5).map(m => m.id) // TODO: better
 }
-
