f14de05991c490e5805d06c6a8382294518fafd3
Aram Drevekenin <grimsniffer@gmail.com>
2018-08-06 14:37:12 +0200

    refactor(text-manipulation): remove unneeded functions

--- js/pipeline/text-manipulation.js
+++ js/pipeline/text-manipulation.js
@@ -2,13 +2,10 @@ const {
   append,
   compose,
   curry,
-  remove,
   prop,
   length,
   last,
-  path,
   find,
-  insertAll,
   join,
   add,
   split,
@@ -26,14 +23,6 @@ const appendToString = char => compose(
   append(char)
 )
 
-const replaceSearchterm = curry((searchterm, suggestions, selection) => {
-  const removed = removeSearchtermFromText(searchterm, selection)
-  return compose(
-    join(''),
-    insertChosenWordIntoText(searchterm, selection, suggestions)
-  )(removed)
-})
-
 const calculateCursorPosition = curry((searchterm, suggestions, selection) => {
   return add(
     compose(length, extractChosenWord)(suggestions),
@@ -41,51 +30,25 @@ const calculateCursorPosition = curry((searchterm, suggestions, selection) => {
   )
 })
 
-const appendToLastWord = (pos, char) => compose(
+const appendToLastWord = curry((pos, char) => compose(
   appendToString(char),
   lastWordBeforeIndex(pos)
-)
-
-const lastWordUpToCursor = (selection, char) => {
-  const { anchorNode, anchorOffset } = selection
-  const { textContent } = anchorNode
-  const appendChar = appendToLastWord(anchorOffset, char)
-  return appendChar(textContent)
-}
+))
 
 const extractChosenWord = compose(
   prop('word'),
   find(prop('selected'))
 )
 
-const insertChosenWordIntoText = curry(
-  (searchterm, selection, suggestions, text) => {
-    const startPos = findStartOfWord(selection, searchterm)
-    const chosenSuggestion = extractChosenWord(suggestions)
-    return insertAll(startPos, chosenSuggestion, text)
-  }
-)
-
 const findStartOfWord = curry((selection, searchterm) => {
   return selection.anchorOffset - searchterm.length
-})
-
-const removeSearchtermFromText = curry((searchterm, selection) => {
-  const startPos = findStartOfWord(selection, searchterm)
-  const searchtermEndPos = length(searchterm)
-  const origText = path(['anchorNode', 'textContent'], selection)
-  return remove(startPos, searchtermEndPos, origText)
 })
 
 module.exports = {
   lastWordBeforeIndex,
   appendToString,
-  replaceSearchterm,
   calculateCursorPosition,
   appendToLastWord,
-  lastWordUpToCursor,
   extractChosenWord,
-  insertChosenWordIntoText,
-  findStartOfWord,
-  removeSearchtermFromText
+  findStartOfWord
 }
